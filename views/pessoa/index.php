ea5e6f78ab7d33f63027b6de829808456e48e438
mrlucasrocha <lucasoliveira.rocha@hotmail.com>
2019-05-10 11:03:48 -0300

    inserindo modificação

--- views/pessoa/index.php
+++ views/pessoa/index.php
@@ -7,7 +7,7 @@ use yii\grid\GridView;
 /* @var $searchModel app\models\PessoaSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Pessoas teste git';
+$this->title = 'Pessoas teste git git volta pessoa teste';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="pessoa-index">
